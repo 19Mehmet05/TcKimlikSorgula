2f88539dba6c2d278755a1e48dd93eb38251c725
Mehmet1905 <mehmet.buykbayram@gmail.com>
2021-04-02 14:13:39 +0300

    ilk gönderme

---
+++ MernisProje/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MernisProje
+{
+    class Items
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string TcKimlikNo { get; set; }
+    }
+}
---
+++ MernisProje/TcSorgulama.cs
@@ -0,0 +1,31 @@
+﻿using MernisProje.TcServis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MernisProje
+{
+    class TcSorgulama
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public string TcKimlikNo { get; set; }
+        private KPSPublicSoapClient client = new KPSPublicSoapClient();
+
+        public bool CheckIfRealPerson()
+        {
+            Isim = "Mehmet";
+            Soyisim = "Büyükbayram";
+            DogumTarihi = new DateTime(2000, 1, 1);
+            TcKimlikNo = "11639673087";
+            return client.TCKimlikNoDogrula(
+                Convert.ToInt64(TcKimlikNo),
+                Isim.ToUpper(),
+                Soyisim.ToUpper(), DogumTarihi.Year);
+
+        }
+    }
+}
